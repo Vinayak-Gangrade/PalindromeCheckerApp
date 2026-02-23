7123d01b5ea6a9e766bb61a3a149e143dca02563
Vinayak-Gangrade <gangradevinayak001@gmail.com>
2026-02-23 16:15:02 +0530

    UC8:Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,68 @@
-// UseCase7PalindromeCheckerApp.java
+// UseCase8PalindromeCheckerApp.java
 
-import java.util.*;
+import java.util.Scanner;
 
-class UseCase7PalindromeCheckerApp {
+class UseCase8PalindromeCheckerApp {
+
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node head = null;
+
+    static void insert(char data) {
+        Node newNode = new Node(data);
+        if (head == null) head = newNode;
+        else {
+            Node temp = head;
+            while (temp.next != null) temp = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+    static Node reverse(Node node) {
+        Node prev = null, current = node, next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    static boolean isPalindrome() {
+
+        if (head == null || head.next == null) return true;
+
+        Node slow = head, fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow.next);
+        Node firstHalf = head;
+
+        Node temp = secondHalf;
+        boolean result = true;
+
+        while (result && temp != null) {
+            if (firstHalf.data != temp.data)
+                result = false;
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+
+        return result;
+    }
 
     public static void main(String[] args) {
 
@@ -10,24 +70,9 @@ class UseCase7PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String text = sc.nextLine();
 
-        Deque<Character> deque = new ArrayDeque<>();
+        for (char ch : text.toCharArray()) insert(ch);
 
-        // Insert characters into deque
-        for (char ch : text.toCharArray()) {
-            deque.addLast(ch);
-        }
-
-        boolean isPalindrome = true;
-
-        // Remove first & last and compare
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome)
+        if (isPalindrome())
             System.out.println("It is a Palindrome");
         else
             System.out.println("It is NOT a Palindrome");
